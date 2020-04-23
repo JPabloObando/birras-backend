13c03a38f67acf1f6c6b7954c9e8501a138e6528
Pablo Obando <pobando@bananacode.co>
2020-04-23 00:59:03 -0600

    First deploy in Heroku

---
+++ src/resolvers/Query.js
@@ -0,0 +1,5 @@
+const test = () => "Hello Word!";
+
+module.exports = {
+  test,
+};
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+
+const start = () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+  });
+
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+};
+
+module.exports = start;
