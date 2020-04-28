4da33e4feda64845164565ce9c9108e03fe87360
Pablo Obando <pobando@bananacode.co>
2020-04-28 02:57:45 -0600

    Testing with env

--- server.js
+++ server.js
@@ -1,47 +1,29 @@
-require("dotenv").config();
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const typeDefs = require("./src/schema");
-const resolvers = require("./src/resolvers");
-const fs = require("fs");
-const https = require("https");
-const http = require("http");
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import typeDefs from "./src/schema";
+import resolvers from "./src/resolvers";
+import fs from "fs";
+import https from "https";
+import http from "http";
 
-const configurations = {
-  production: {
-    ssl: true,
-    port: process.env.PORT || 4000,
-    hostname: "example.com",
-  },
-  development: {
-    ssl: false,
-    port: process.env.PORT || 4000,
-    hostname: "localhost",
-  },
-};
-
-const environment = process.env.NODE_ENV || "development";
-const config = configurations[environment];
-
-const app = express();
-
-const apolloServer = new ApolloServer({
+const apollo = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
   playground: true,
 });
 
-apolloServer.applyMiddleware({ app });
+const app = express();
+apollo.applyMiddleware({ app });
 
-const sslServer = https
-  .createServer(
-    {
-      key: fs.readFileSync(`./ssl/server.key`),
-      cert: fs.readFileSync(`./ssl/server.crt`),
-    },
-    app
-  )
-  .listen({ port: config.port }, function () {
-    console.log(`Ready dudes`);
-  });
+server = https.createServer(
+  {
+    key: fs.readFileSync(`./ssl/server.key`),
+    cert: fs.readFileSync(`./ssl/server.crt`),
+  },
+  app
+);
+
+server.listen({ port: process.env.PORT || 3000 }, () =>
+  console.log("🚀 Server ready at")
+);
