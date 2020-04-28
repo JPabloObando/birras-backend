4ed7aa658d2c2cebcc66f47af5d9c56550022ea2
Pablo Obando <pobando@bananacode.co>
2020-04-28 02:34:51 -0600

    Testing ssl config

--- server.js
+++ server.js
@@ -1,74 +1,56 @@
 require("dotenv").config();
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-const bodyParser = require("body-parser");
+const typeDefs = require("./src/schema");
+const resolvers = require("./src/resolvers");
 const fs = require("fs");
 const https = require("https");
 const http = require("http");
-const db = require("./src/db");
-const typeDefs = require("./src/schema");
-const resolvers = require("./src/resolvers");
-const { auth } = require("./src/utils");
-const port = process.env.PORT || 4000;
-const environment = process.env.NODE_ENV || "development";
 
 const configurations = {
   production: {
     ssl: true,
-    port,
-    url: "https://birras-backend.herokuapp.com/graphql",
+    port: process.env.PORT || 4000,
+    hostname: "example.com",
   },
   development: {
     ssl: false,
-    port,
-    url: `http://localhost:${port}/graphql`,
+    port: process.env.PORT || 4000,
+    hostname: "localhost",
   },
 };
+
+const environment = process.env.NODE_ENV || "development";
 const config = configurations[environment];
 
-/**
- * @func context
- * @desc Share across all resolvers a potential user and an initialization of our db
- */
-const context = async ({ req, res }) => {
-  const user = await auth(req, res);
-  return { user, db };
-};
+const apollo = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
-/**
- * @func start
- * @desc Create an instance of ApolloServer and then will launch a web server
- */
-const start = () => {
-  const app = express();
+const app = express();
+apollo.applyMiddleware({ app });
 
-  const apollo = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context,
-    introspection: true,
-    playground: true,
-  });
-
-  app.use(bodyParser.json({ limit: "100mb" }));
-  apollo.applyMiddleware({ app, path: "/" });
-
-  let server = null;
-  if (config.ssl) {
-    server = https.createServer(
-      {
-        key: fs.readFileSync(`./ssl/server.key`),
-        cert: fs.readFileSync(`./ssl/server.crt`),
-      },
-      app
-    );
-  } else {
-    server = http.createServer(app);
-  }
-
-  server.listen({ port: config.port }, () =>
-    console.log(`🚀 Server ready at ${config.url}`)
+let server = null;
+if (config.ssl) {
+  server = https.createServer(
+    {
+      key: fs.readFileSync(`./ssl/server.key`),
+      cert: fs.readFileSync(`./ssl/server.crt`),
+    },
+    app
   );
-};
+} else {
+  server = http.createServer(app);
+}
 
-start();
+server.listen({ port: config.port }, () =>
+  console.log(
+    "🚀 Server ready at",
+    `http${config.ssl ? "s" : ""}://${config.hostname}:${config.port}${
+      apollo.graphqlPath
+    }`
+  )
+);
